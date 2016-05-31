cedc462ef9d18507734b261234950fca59170cfb
Tim Reynolds <timbreynolds@gmail.com>
2016-05-30 22:29:03 -0400

    Get rid of unnecessary Get method.

--- src/models/event.py
+++ src/models/event.py
@@ -4,10 +4,3 @@ class Event(ndb.Model):
   name = ndb.StringProperty()
   priority = ndb.IntegerProperty()
   is_real = ndb.BooleanProperty()
-
-  @staticmethod
-  def Get(event_id):
-    events = Event.query(Event.id == event_id).iter()
-    if not events:
-      return None
-    return events[0]
