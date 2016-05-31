0fc79eff9306810cf9c8c2fc9ae595b99ee665d1
Tim Reynolds <timbreynolds@gmail.com>
2016-05-30 22:37:54 -0400

    Fix basic handler.

--- src/schedule.py
+++ src/schedule.py
@@ -6,9 +6,5 @@ from src.models import Competitor
 
 class GetSchedule(webapp2.RequestHandler):
   def get(self, person_id):
-    competitor_query = Competitor.query(Competitor.reg_id == int(person_id)).fetch(1)
-    if not competitor_query:
-      self.response.write('No competitor found')
-      return
-    competitor = competitor_query[0]
+    competitor = Competitor.get_by_id(person_id)
     self.response.write(competitor.name)
