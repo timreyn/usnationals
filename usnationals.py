e342a1f67a21b3dac184fa292273dfbb620b9fa8
Tim Reynolds <timbreynolds@gmail.com>
2016-06-01 19:40:48 -0400

    Fix regexp in get_schedule route.

--- usnationals.py
+++ usnationals.py
@@ -4,6 +4,6 @@ from src import admin
 from src import schedule
 
 app = webapp2.WSGIApplication([
-    webapp2.Route('/get_schedule/(\d*)', handler=schedule.GetSchedule),
+    webapp2.Route(r'/get_schedule/<person_id:\d*>', handler=schedule.GetSchedule, name='get_schedule'),
     webapp2.Route('/admin/add_data', handler=admin.AddData, name='add_data'),
 ], debug=True)
