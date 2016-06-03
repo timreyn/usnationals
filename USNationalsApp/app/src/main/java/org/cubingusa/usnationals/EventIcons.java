791270288f75eaeef7fa19fbfde039f2efa6ae9a
Tim Reynolds <timbreynolds@gmail.com>
2016-06-03 19:04:34 -0400

    Add option to EventIcons to get resource ids.

--- USNationalsApp/app/src/main/java/org/cubingusa/usnationals/EventIcons.java
+++ USNationalsApp/app/src/main/java/org/cubingusa/usnationals/EventIcons.java
@@ -8,34 +8,43 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class EventIcons {
-    private Map<String, Drawable> drawableMap;
+    private final Context context;
+    private Map<String, Integer> drawableMap;
 
     public EventIcons(Context context) {
-        drawableMap = new HashMap<String, Drawable>();
-        drawableMap.put("222", ContextCompat.getDrawable(context, R.drawable.e_222));
-        drawableMap.put("333", ContextCompat.getDrawable(context, R.drawable.e_333));
-        drawableMap.put("333bf", ContextCompat.getDrawable(context, R.drawable.e_333bf));
-        drawableMap.put("333fm", ContextCompat.getDrawable(context, R.drawable.e_333fm));
-        drawableMap.put("333ft", ContextCompat.getDrawable(context, R.drawable.e_333ft));
-        drawableMap.put("333mbf", ContextCompat.getDrawable(context, R.drawable.e_333mbf));
-        drawableMap.put("333oh", ContextCompat.getDrawable(context, R.drawable.e_333oh));
-        drawableMap.put("444", ContextCompat.getDrawable(context, R.drawable.e_444));
-        drawableMap.put("444bf", ContextCompat.getDrawable(context, R.drawable.e_444bf));
-        drawableMap.put("555", ContextCompat.getDrawable(context, R.drawable.e_555));
-        drawableMap.put("555bf", ContextCompat.getDrawable(context, R.drawable.e_555bf));
-        drawableMap.put("666", ContextCompat.getDrawable(context, R.drawable.e_666));
-        drawableMap.put("777", ContextCompat.getDrawable(context, R.drawable.e_777));
-        drawableMap.put("clock", ContextCompat.getDrawable(context, R.drawable.e_clock));
-        drawableMap.put("minx", ContextCompat.getDrawable(context, R.drawable.e_minx));
-        drawableMap.put("pyram", ContextCompat.getDrawable(context, R.drawable.e_pyram));
-        drawableMap.put("skewb", ContextCompat.getDrawable(context, R.drawable.e_skewb));
-        drawableMap.put("sq1", ContextCompat.getDrawable(context, R.drawable.e_sq1));
+        this.context = context;
+        drawableMap = new HashMap<String, Integer>();
+        drawableMap.put("222", R.drawable.e_222);
+        drawableMap.put("333", R.drawable.e_333);
+        drawableMap.put("333bf", R.drawable.e_333bf);
+        drawableMap.put("333fm", R.drawable.e_333fm);
+        drawableMap.put("333ft", R.drawable.e_333ft);
+        drawableMap.put("333mbf", R.drawable.e_333mbf);
+        drawableMap.put("333oh", R.drawable.e_333oh);
+        drawableMap.put("444", R.drawable.e_444);
+        drawableMap.put("444bf", R.drawable.e_444bf);
+        drawableMap.put("555", R.drawable.e_555);
+        drawableMap.put("555bf", R.drawable.e_555bf);
+        drawableMap.put("666", R.drawable.e_666);
+        drawableMap.put("777", R.drawable.e_777);
+        drawableMap.put("clock", R.drawable.e_clock);
+        drawableMap.put("minx", R.drawable.e_minx);
+        drawableMap.put("pyram", R.drawable.e_pyram);
+        drawableMap.put("skewb", R.drawable.e_skewb);
+        drawableMap.put("sq1", R.drawable.e_sq1);
     }
 
     public Drawable getDrawable(String eventId) {
         if (drawableMap.containsKey(eventId)) {
-            return drawableMap.get(eventId);
+            return ContextCompat.getDrawable(context, drawableMap.get(eventId));
         }
         return null;
+    }
+
+    public int getDrawableId(String eventId) {
+        if (drawableMap.containsKey(eventId)) {
+            return drawableMap.get(eventId);
+        }
+        return -1;
     }
 }
