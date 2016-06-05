1b4d43bc6e72b5a145690311b557a8da19d26820
Tim Reynolds <timbreynolds@gmail.com>
2016-06-05 17:37:41 -0400

    Randomly assign judging stations.

--- usnationals.py
+++ usnationals.py
@@ -1,6 +1,7 @@
 import webapp2
 
 from src import admin
+from src import assign_stations
 from src import get_competitors
 from src import get_stages
 from src import register_device
@@ -16,6 +17,7 @@ app = webapp2.WSGIApplication([
     webapp2.Route('/get_stages', handler=get_stages.GetStages),
     webapp2.Route('/register_device', handler=register_device.RegisterDevice),
     webapp2.Route('/admin/add_data', handler=admin.AddData, name='add_data'),
+    webapp2.Route('/admin/assign_stations', handler=assign_stations.AssignStations),
     webapp2.Route('/admin/edit_users', handler=admin.EditUsers, name='edit_users'),
     webapp2.Route(r'/admin/set_firebase_key/<key:.*>', handler=admin.SetFirebaseKey),
     webapp2.Route(r'/admin/send_notification' +
