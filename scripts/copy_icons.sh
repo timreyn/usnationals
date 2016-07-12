b4166430fda330d32a15e4f428cf37f9eefdc96e
Tim Reynolds <timbreynolds@gmail.com>
2016-07-11 21:37:44 -0400

    Make image assets smaller.

--- scripts/copy_icons.sh
+++ scripts/copy_icons.sh
@@ -4,7 +4,7 @@ TARGET=../USNationalsApp/app/src/main/res/drawable
 for FILE in $(ls $SRC)
 do
   FILENAME=$(echo $FILE | sed -s "s/\.svg//")
-  convert $SRC/$FILENAME.svg $TARGET/e_$FILENAME.png
+  convert -resize 50x50 $SRC/$FILENAME.svg $TARGET/e_$FILENAME.png
   convert -background transparent $SRC/$FILENAME.svg \
-          -fill white -opaque black $TARGET/e_${FILENAME}_transparent.png
+          -fill white -opaque black -resize 50x50 $TARGET/e_${FILENAME}_transparent.png
 done
