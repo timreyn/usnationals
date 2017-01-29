b72f3c5e7c601d85eb31825c9cee224f0b88ff23
Tim Reynolds <timbreynolds@gmail.com>
2017-01-29 12:28:06 -0500

    List competitors in heats, and clean up CSS.

--- src/admin/assignments/formats.py
+++ src/admin/assignments/formats.py
@@ -2,3 +2,6 @@
 
 def FormatHeatStartTime(start_time):
   return start_time.strftime('%a %H:%M')
+
+def FormatCompetitorBirthday(birthday):
+  return birthday.strftime('%Y-%m-%d')
---
+++ src/static/common.js
@@ -0,0 +1,8 @@
+function toggle(id) {
+  var div = document.getElementById(id);
+  if (div.style.display == "none") {
+    div.style.display = "block";
+  } else {
+    div.style.display = "none";
+  }
+}
