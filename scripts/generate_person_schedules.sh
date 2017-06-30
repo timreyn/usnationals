5d6fccb4e19835dadd46bc0d054c87c580322cff
Tim Reynolds <timbreynolds@gmail.com>
2017-06-30 15:09:21 -0400

    Sort person schedules by staff, then lastname.

--- scripts/generate_person_schedules.sh
+++ scripts/generate_person_schedules.sh
@@ -1,6 +1,8 @@
 
-COMPETITOR_NUM=1
-while [ $COMPETITOR_NUM -lt 777 ]; do
+for COMPETITOR_NUM in $(cat $(dirname $0)/nonstaff_ids); do
   wget -O- https://www.cubingusa.com/nationals2017/groups.php?i=$COMPETITOR_NUM\&printable >> $1
-  COMPETITOR_NUM=$[ $COMPETITOR_NUM + 1]
+done
+
+for COMPETITOR_NUM in $(cat $(dirname $0)/staff_ids); do
+  wget -O- https://www.cubingusa.com/nationals2017/groups.php?i=$COMPETITOR_NUM\&printable >> $1
 done
