30e54e92fb2e068abec2cceedcb8ed76746f2ec1
ToastyKen <kenlu@subjunctive.net>
2017-07-01 23:25:59 -0700

    Fix case-sensitive variable names

--- scripts/generate_station_schedules.sh
+++ scripts/generate_station_schedules.sh
@@ -1,6 +1,6 @@
-for stage in r g b o; do
-  for station in 1 2 3 4 5 6 7 8; do
-    wget -O- https://usnationals2017.appspot.com/job_schedule?job=J\&stage=$STAGE\&station=$station >> $1
+for STAGE in r g b o; do
+  for STATION in 1 2 3 4 5 6 7 8; do
+    wget -O- https://usnationals2017.appspot.com/job_schedule?job=J\&stage=$STAGE\&station=$STATION >> $1
   done
   wget -O- https://usnationals2017.appspot.com/job_schedule?job=R\&stage=$STAGE >> $1
   wget -O- https://usnationals2017.appspot.com/job_schedule?job=S\&stage=$STAGE >> $1
