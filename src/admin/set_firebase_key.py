bc8cd0e000d4e8e126561b466397043f0c97ddc3
Tim Reynolds <timbreynolds@gmail.com>
2017-07-03 11:38:06 -0400

    Add a comment that set_firebase_key is dumb.

--- src/admin/set_firebase_key.py
+++ src/admin/set_firebase_key.py
@@ -5,6 +5,7 @@ from src.models import FirebaseKey
 
 class SetFirebaseKey(webapp2.RequestHandler):
   # This should really be a POST...but it's easier this way.
+  # Or really this should just be part of add_data.py, like everything else...
   def get(self, key):
     firebase_key = FirebaseKey.get_by_id('1') or FirebaseKey(id = '1')
     firebase_key.f_key = key
