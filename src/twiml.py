29608632373c9e71ddf024b2b641e16574082a2d
Tim Reynolds <timbreynolds@gmail.com>
2018-07-21 18:50:26 -0400

    Switch to using WCA ID for SMS.

--- src/twiml.py
+++ src/twiml.py
@@ -13,24 +13,24 @@ class Twiml(webapp2.RequestHandler):
       if num_subscriptions >= 5:
         self.response.write("Sorry, you can only subscribe to 5 competitors per phone number!")
         return
-      competitor = Competitor.get_by_id(msg_split[-1])
+      wca_id = msg_split[-1]
+      competitor = Competitor.query(Competitor.wca_id == wca_id)
       if not competitor:
-        self.response.write('Sorry, I can\'t find a competitor with ID ' + msg_split[-1])
+        self.response.write('Sorry, I can\'t find a competitor with ID ' + wca_id)
         return
       num_subscriptions = len([s for s in SMSSubscriber.query(SMSSubscriber.competitor == competitor.key).iter()])
       if num_subscriptions >= 2:
         self.response.write("Sorry, only 2 phone numbers can subscribe to each competitor!")
         return
-      subscriber_id = '%s_%s' % (phone_number, msg_split[-1])
+      subscriber_id = '%s_%s' % (phone_number, wca_id)
       subscriber = SMSSubscriber.get_by_id(subscriber_id) or SMSSubscriber(id = subscriber_id)
       subscriber.phone_number = phone_number
       subscriber.competitor = competitor.key
       subscriber.put()
       self.response.write("Subscribed to texts for %s.  Respond \"unsubscribe %s\" to stop." % (
-                              competitor.name, msg_split[-1]))
+                              competitor.name, wca_id))
       return
     if msg_split[0].lower() == 'unsubscribe':
-      competitor = Competitor.get_by_id(msg_split[-1])
       subscriber = SMSSubscriber.get_by_id('%s_%s' % (phone_number, msg_split[-1]))
       if subscriber:
         subscriber.key.delete()
