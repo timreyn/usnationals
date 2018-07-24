ebc143d864632764bec51b597f96896345c0e5ea
Tim Reynolds <timbreynolds@gmail.com>
2018-07-23 22:37:31 -0400

    Fix year in generate_station_schedules.

--- scripts/generate_station_schedules.sh
+++ scripts/generate_station_schedules.sh
@@ -1,11 +1,10 @@
 for STAGE in r g b o; do
-  for STATION in 1 2 3 4 5 6 7 8; do
-    wget -O- https://usnationals2017.appspot.com/job_schedule?job=J\&stage=$STAGE\&station=$STATION >> $1
+  for STATION in 1 2 3 4 5 6 7 8 9 10; do
+    wget -O- https://usnationals2018.appspot.com/job_schedule?job=J\&stage=$STAGE\&station=$STATION >> $1
   done
-  wget -O- https://usnationals2017.appspot.com/job_schedule?job=R\&stage=$STAGE >> $1
-  wget -O- https://usnationals2017.appspot.com/job_schedule?job=S\&stage=$STAGE >> $1
+  wget -O- https://usnationals2018.appspot.com/job_schedule?job=R\&stage=$STAGE >> $1
+  wget -O- https://usnationals2018.appspot.com/job_schedule?job=S\&stage=$STAGE >> $1
 done
-wget -O- https://usnationals2017.appspot.com/job_schedule?job=H >> $1
-wget -O- https://usnationals2017.appspot.com/job_schedule?job=D >> $1
-wget -O- https://usnationals2017.appspot.com/job_schedule?job=L >> $1
-wget -O- https://usnationals2017.appspot.com/job_schedule?job=U >> $1
+wget -O- https://usnationals2018.appspot.com/job_schedule?job=H >> $1
+wget -O- https://usnationals2018.appspot.com/job_schedule?job=L >> $1
+wget -O- https://usnationals2018.appspot.com/job_schedule?job=U >> $1
