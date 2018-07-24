5853edb196d1c6d3fbf1f9e49a6900b3d5007011
Tim Reynolds <timbreynolds@gmail.com>
2018-07-23 21:24:24 -0400

    Don't use https for cubecomps.

--- src/cubecomps.py
+++ src/cubecomps.py
@@ -3,6 +3,6 @@ import webapp2
 class CubecompsRedirect(webapp2.RequestHandler):
   def get(self, person_id=''):
     if person_id:
-      self.redirect('https://m.cubecomps.com/competitions/3381/competitors/' + person_id)
+      self.redirect('http://m.cubecomps.com/competitions/3381/competitors/' + person_id)
     else:
-      self.redirect('https://m.cubecomps.com/competitions/3381')
+      self.redirect('http://m.cubecomps.com/competitions/3381')
