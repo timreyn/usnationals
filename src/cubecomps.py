ef00badd9b527c486d7d6a82d5aeaf60e84a2a6d
Tim Reynolds <timbreynolds@gmail.com>
2018-07-23 20:40:05 -0400

    Add cubecomps server-side redirect.

--- src/cubecomps.py
+++ src/cubecomps.py
@@ -2,5 +2,7 @@ import webapp2
 
 class CubecompsRedirect(webapp2.RequestHandler):
   def get(self, person_id=''):
-    # TODO: change this to the cubecomps path for the competition.
-    self.redirect('https://m.cubecomps.com/')
+    if person_id:
+      self.redirect('https://m.cubecomps.com/competitions/3381/competitors/' + person_id)
+    else:
+      self.redirect('https://m.cubecomps.com/competitions/3381')
